307c025e07900efcb255ff251ec5eea511a4e578
o-yelkoff <o.yelkoff@gmail.com>
2023-06-09 23:09:41 +0300

    git 6

--- src/Main.java
+++ src/Main.java
@@ -1,13 +1,6 @@
 public class Main {
+
     public static void main(String[] args) {
-        System.out.println("Hello world");
-
-        System.out.println("Git gandony");
-
-        System.out.println("Git gandony 3");
-
-        System.out.println("Git gandony 4");
-
-        System.out.println("Git gandony 5");
+        System.out.println("Hello world!");
     }
 }
---
+++ src/Pack1/Less1.java
@@ -0,0 +1,12 @@
+package Pack1;
+
+public class Less1 {
+    public static void main(String[] args) {
+        for (int i = 0; i < 5; i++) {
+            System.out.println(i);
+
+        }
+        System.out.println("YRA");
+    }
+
+}
