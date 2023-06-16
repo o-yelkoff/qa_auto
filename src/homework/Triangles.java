50b0c86f3b93b92e4655218aa00af7bad511a319
OleksiiYelkov <o.yelkoff@gmail.com>
2023-06-16 23:45:43 +0300

    Home work #3

---
+++ src/homework/Triangles.java
@@ -0,0 +1,12 @@
+package homework;
+
+public class Triangles {
+    public static void main(String[] args) {
+        for (int i = 0; i < 5; i++) {
+            for (int j = i; j >=0 ; j--) {
+                System.out.print("*");
+            }
+            System.out.println();
+            }
+        }
+    }
