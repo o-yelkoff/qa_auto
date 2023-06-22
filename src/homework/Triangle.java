d478a4e38dba8d730e3186a5a141f233db82ede0
o-yelkoff <o.yelkoff@gmail.com>
2023-06-22 09:22:27 +0300

    Home work 3

--- src/homework/Triangle.java
+++ src/homework/Triangle.java
@@ -19,8 +19,8 @@ public class Triangle {
     }
 
     static void triangle2(int n) {
-        for (int i = n; i >=1 ; i--) {
-            for (int j = 0; j < n-i; j++) {
+        for (int i = n; i >= 1; i--) {
+            for (int j = 0; j < n - i; j++) {
                 System.out.print(" ");
             }
             for (int j = 0; j < i; j++) {
@@ -29,9 +29,10 @@ public class Triangle {
             System.out.println();
         }
     }
-    static void triangle3(int n){
-        for (int i = 1; i <= n ; i++) {
-            for (int j = 0; j < n-i; j++) {
+
+    static void triangle3(int n) {
+        for (int i = 1; i <= n; i++) {
+            for (int j = 0; j < n - i; j++) {
                 System.out.print(" ");
             }
             for (int j = 0; j < i; j++) {
