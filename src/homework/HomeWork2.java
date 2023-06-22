1049e82cd4598413286a5f85679027bb0686301a
o-yelkoff <o.yelkoff@gmail.com>
2023-06-22 09:18:36 +0300

    Home work 2

--- src/homework/HomeWork2.java
+++ src/homework/HomeWork2.java
@@ -21,15 +21,12 @@ public class HomeWork2 {
 
     static int findSum(int inputnum) {
         int sum = 0;
-        while (inputnum != 0) {
-            sum = sum + inputnum % 10;
-            inputnum = inputnum / 10;
-        }
+        for (sum = 0; inputnum > 0; sum = sum + inputnum % 10, inputnum = inputnum / 10);
         return sum;
     }
 
     public static void main(String args[]) {
-        int inputnum = 348;
+        int inputnum = 222;
         long l = inputnum;
         String str1 = "Mickey";
         String str2 = "Mouse";
