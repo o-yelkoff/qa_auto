8d1af0de17c7660dfa4cfe59ed2c524f7cc1b633
o-yelkoff <o.yelkoff@gmail.com>
2023-06-25 21:08:38 +0300

    Home work 4

--- src/homework/StudentTest.java
+++ src/homework/StudentTest.java
@@ -1,9 +1,9 @@
 package homework;
 
 public class StudentTest {
-    static Student student1 = new Student("XX123BB", "Alex", "Yelkoff", 2, 2.9, 2.9, 2.9);
-    static Student student2 = new Student("XX123BB", "Dmytro", "Lytcenko", 2, 4.0, 3.7, 5);
-    static Student student3 = new Student("XX123BB", "Vitalii", "Nemudrui", 2, 4.5, 3.5, 4.5);
+    static Student student1 = new Student("XX123BB", "Alex", "Yelkoff", 2, 4.5, 3.1, 3.9);
+    static Student student2 = new Student("XX465BB", "Dmytro", "Lytcenko", 2, 4.0, 3.7, 5);
+    static Student student3 = new Student("XX789BB", "Vitalii", "Nemudrui", 2, 4.5, 3.5, 4.5);
 
     public static double studentAveregeValue(Student student){
         return ((student.gpaFromEconomy+student.gpaFromMath+student.gpaFromLanguage)/3);
--- src/homework2/EmployeeTest.java
+++ src/homework2/EmployeeTest.java
@@ -4,12 +4,12 @@ import homework.Employee;
 
 public class EmployeeTest {
     public static void main(String[] args) {
-        Employee employee = new Employee("Prigojun", 21);
+        Employee employee = new Employee("Yelkoff", 21);
         System.out.println(employee.getId());
         System.out.println(employee.getSalary());
         System.out.println(employee.getSureName());
         
-        employee = new Employee("Prigojun2");
+        employee = new Employee("Yelkoff2");
         System.out.println(employee.getId());
         System.out.println(employee.getSalary());
         System.out.println(employee.getSureName());
